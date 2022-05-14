b236be416650ef6ad73c015a84177489e9cff665
Robin Li <40032541+Roow@users.noreply.github.com>
2022-05-15 00:27:34 +1000

    graphs all done

--- PSK.m
+++ PSK.m
@@ -7,48 +7,79 @@ clc; clear all;close all
 M = 8;                % No. of symbols [2 4 8]
 fc = 100000;                  % carrier frequency
 eb = 1;                       % energy per bit
-es = eb*log2(M);              % energy per symbol
 T = 0.0001;                   % symbol duration
 t = linspace(0, T, 1000);     % time vector
 symbol_length = log2(M);      % bits/symbol
 symbol_plot = 4;              % how many symbols to include in PSK plot
 
 
-%% Message frame encoding
-% random 1024 bits
-bit_frame = randi([0, 1],1,1024);
-
-% 1024 bits converted into symbols
-symbol_frame = encoder(bit_frame, M);
-
-
 %% Time domain plot of PSK modulated waveform
-transmitted_signal = [];%composite of all symbols
-signal_length = [];%total time vector
+for M = [2, 4, 8]
 
-% look at only first few symbols
-for j = 1:symbol_plot
+    es = eb*log2(M); % energy per symbol
+
+    % random 1024 bits
+    bit_frame = randi([0, 1],1,1024);
     
-    % determine phase of symbol
-    i = symbol_frame(j); % s_i using phi_i 
-    phi = 2*pi.*i/M;
+    % 1024 bits converted into symbols
+    symbol_frame = encoder(bit_frame, M);
+    
+    transmitted_signal = [];%composite of all symbols
+    signal_length = [];%total time vector
+    
+    % look at only first few symbols
+    for j = 1:symbol_plot
+        
+        % determine phase of symbol
+        i = symbol_frame(j); % s_i using phi_i 
+        phi = 2*pi.*i/M;
+    
+        % waveform of one symbol
+        pskSignal = sqrt(2*es/T)*cos(2*pi*fc.*t - phi); 
+    
+        % adding the symbol to the final output signal
+        transmitted_signal = [transmitted_signal, pskSignal]; 
+    end
+    
+    %scale signal down
+    transmitted_signal = transmitted_signal * 0.01;
+    
+    %total signal length = (1024/symbol_length) * 1*10^-4 s
+    
+    signal_length = linspace(0, symbol_plot*T, 1000*symbol_plot);
 
-    % waveform of one symbol
-    pskSignal = sqrt(2*es/T)*cos(2*pi*fc.*t - phi); 
+    subplot(3, 1, log2(M))
+    plot(1000*signal_length,transmitted_signal)
+    title(sprintf('%d-PSK Modulation', M))
 
-    % adding the symbol to the final output signal
-    transmitted_signal = [transmitted_signal, pskSignal]; 
+    %xlim([0 T])
 end
 
-%scale signal down
-transmitted_signal = transmitted_signal * 0.01;
 
-%total signal length = (1024/symbol_length) * 1*10^-4 s
 
-signal_length = linspace(0, symbol_plot*T, 1000*symbol_plot);
-plot(signal_length,transmitted_signal)
-hold on
-xlim([0 T])
+
+%% Plotting Bit/Symbol probability vs Eb/N0
+ebn0_db = 0:1:10;
+ebn0_lin = 10.^(ebn0_db/10);
+
+q = @(x) 0.5*erfc(x/sqrt(2));
+
+% 2PSK Pb = 2PSK Ps = 4PSK Pb
+pb2 = q(sqrt(2*ebn0_lin));
+
+% 4PSD Ps
+ps4 = erfc(sqrt(ebn0_lin));
+
+% 8PSK error probability
+ps8 = 2*q(sqrt(2*log2(M)*ebn0_lin)*sin(pi/M));
+pb8 = ps8/log2(M);
+
+figure(3)
+plot(ebn0_db, log10(pb2), ebn0_db, log10(ps4), ebn0_db, log10(ps8), ebn0_db, log10(pb8));
+legend('2PSK Pb = 2PSK Ps = 4PSK Pb', '4PSK Ps', '8PSK Pb', '8PSK Ps');
+xlabel('Eb/N0 (dB)')
+ylabel('Error probability (log10(P))')
+title('Error probability vs Eb/N0 for various PSK modulation schemes')
 
 
 %% Error probability
